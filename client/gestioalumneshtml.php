1b36b9119d30be231670891602f06f1322bf4ac1
mtcraft22 <matort535@gmail.com>
2023-05-08 12:52:15 +0200

    misterio resuelto

--- client/gestioalumneshtml.php
+++ client/gestioalumneshtml.php
@@ -4,8 +4,6 @@ if(!isset($_SESSION["usuari"])){
     header("location:../login/login.php"); 
 }elseif($_SESSION["tipus"]=="Mestre"){
     header("location:../permisos/denegat.php");
-}else{
-    header("location:./gestio.html");
 }
 ?>
 
