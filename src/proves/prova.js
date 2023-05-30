b2d6445f3b9d9d0af8574e9a32a5f45b1b65c584
mtcraft22 <matort535@gmail.com>
2023-05-30 10:44:25 +0200

    0.1.0

--- src/proves/prova.js
+++
@@ -1,6 +0,0 @@
-import { crud } from "../CRUD/crud.js";
-
-parent=document.getElementById("par")
-let classe=new crud("./../servidor/consultes/gestioalumnes.php",parent)
-
-classe.envia({"classe":'i3a',"extreure":true},"getjson")
--- src/servidor/consultes/dadesconexio.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('_HOST_NAME_', 'localhost');
-define('_DATABASE_NAME_', 'assistencies');
-define('_USER_NAME_', 'root');
-define('_DB_PASSWORD', '');
-?>
